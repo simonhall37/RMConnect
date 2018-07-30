ac09fa34b79a4539d62f46db69c55266be95ab10
shall <simonhall37@hotmail.com>
2018-07-30 16:39:20 +0200

    Tests for ReflectionService

---
+++ src/test/java/com/simon/redmine/services/TestReflectionService.java
@@ -0,0 +1,130 @@
+package com.simon.redmine.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.Test;
+
+import com.simon.redmine.domain.Project;
+import com.simon.redmine.domain.TimeEntry;
+
+public class TestReflectionService {
+
+	@Test
+	public void testGetLocalField_shouldReturnString() {
+		TimeEntry parent = new TimeEntry();
+		parent.setComment("This is a test");
+		
+		ReflectionService service =  new ReflectionService();
+		Object out = null;
+		try {
+			out = service.getLocalField(parent, "Comment");
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		
+		assertThat("This is a test").isEqualTo(out);
+	}
+	
+	@Test
+	public void testGetLocalField_shouldReturnNumber() {
+		TimeEntry parent = new TimeEntry();
+		parent.setHours(8);
+		
+		ReflectionService service =  new ReflectionService();
+		Object out = null;
+		try {
+			out = service.getLocalField(parent, "Hours");
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
+		assertThat(8).isEqualTo(out);
+	}
+	
+	@Test(expected = NoSuchMethodException.class)
+	public void testGetLocalField_shouldThrowException() throws NoSuchMethodException {
+		TimeEntry parent = new TimeEntry();
+		parent.setComment("This is a test");
+		
+		ReflectionService service =  new ReflectionService();
+		try {
+			@SuppressWarnings("unused")
+			Object out = service.getLocalField(parent, "noExisto");
+		} catch ( SecurityException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void testGetField_shouldReturnSimpleValue() {
+		TimeEntry parent = new TimeEntry();
+		parent.setComment("This is a test");
+		
+		ReflectionService service =  new ReflectionService();
+		Object out = null;
+		try {
+			out =  service.getField(parent, "Comment");
+		} catch (IllegalArgumentException  e) {
+			fail(e.getMessage());
+		}
+		
+		assertThat("This is a test").isEqualTo(out);
+	}
+	
+	@Test
+	public void testGetField_shouldReturnEmbeddedValue() {
+		TimeEntry parent = new TimeEntry();
+		Project project = new Project();
+		project.setId(1);
+		project.setName("Dummy");
+		parent.setProject(project);
+		
+		ReflectionService service =  new ReflectionService();
+		Object out = null;
+		try {
+			out =  service.getField(parent, "Project.Name");
+		} catch (IllegalArgumentException  e) {
+			fail(e.getMessage());
+		}
+		
+		assertThat("Dummy").isEqualTo(out);
+	}
+	
+	@Test
+	public void testGetField_shouldReturnNull() {
+		TimeEntry parent = new TimeEntry();
+		
+		ReflectionService service =  new ReflectionService();
+		Object out = null;
+		try {
+			out =  service.getField(parent, "Project.Name");
+		} catch (IllegalArgumentException  e) {
+			fail(e.getMessage());
+		}
+		
+		assertThat(out).isNull();
+	}
+	
+	@Test
+	public void testGetField_shouldReturnNullSimple() {
+		TimeEntry parent = new TimeEntry();
+		
+		ReflectionService service =  new ReflectionService();
+		Object out = null;
+		try {
+			out =  service.getField(parent, "noExisto");
+		} catch (IllegalArgumentException  e) {
+			fail(e.getMessage());
+		}
+		
+		assertThat(out).isNull();
+	}
+
+}
