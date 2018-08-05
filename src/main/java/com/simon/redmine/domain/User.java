9cdde517971c590816fe0e041374b1e5f22963c1
sphall <simonhall37@hotmail.com>
2018-08-05 22:51:16 +0200

    Corrected field names

--- src/main/java/com/simon/redmine/domain/User.java
+++ src/main/java/com/simon/redmine/domain/User.java
@@ -3,7 +3,13 @@ package com.simon.redmine.domain;
 public class User {
 
 	private long id;
+	private String login;
 	private String name;
+	private String firstname;
+	private String lastname;
+	private String mail;
+	private String created_on;
+	private String last_login_on;
 	
 	public User() {}
 
@@ -21,6 +27,54 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getCreated_on() {
+		return created_on;
+	}
+
+	public void setCreated_on(String created_on) {
+		this.created_on = created_on;
+	}
+
+	public String getLast_login_on() {
+		return last_login_on;
+	}
+
+	public void setLast_login_on(String last_login_on) {
+		this.last_login_on = last_login_on;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
 	}
 	
 	
