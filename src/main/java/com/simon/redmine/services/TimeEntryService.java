b59a2c59d2c9032d1a6327dcedb076af0a278eaa
sphall <simonhall37@hotmail.com>
2018-08-05 22:52:43 +0200

    Commented out methods, need to be re-written using new RedmineService methods and conditions

--- src/main/java/com/simon/redmine/services/TimeEntryService.java
+++ src/main/java/com/simon/redmine/services/TimeEntryService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.simon.redmine.domain.TimeEntry;
-import com.simon.redmine.domain.TimeEntryWrapper;
 
 @Service
 public class TimeEntryService {
@@ -25,63 +24,63 @@ public class TimeEntryService {
 	private String format;
 	private static final Logger log = LoggerFactory.getLogger(TimeEntryService.class);
 
-	public List<TimeEntry> getEntriesByDate(String startDateString, String endDateString) {
+//	public List<TimeEntry> getEntriesByDate(String startDateString, String endDateString) {
+//
+//		List<TimeEntry> out = new ArrayList<>();
+//		
+//		LocalDate startDate = null, endDate = null;
+//
+//		try {
+//			startDate = parseDate(startDateString);
+//			endDate = parseDate(endDateString);
+//		} catch (IllegalArgumentException e) {
+//			log.warn(e.getMessage());
+//			return null;
+//		}
+//
+//		List<String> params = new ArrayList<>();
+//		params.add("spent_on=>=" + startDateString);
+//		params.add("spent_on=<=" + endDateString);
+//		params.add("limit=" + limit);
+//		TimeEntryWrapper wrapper = RMService.getResponse(format, "time_entries", params, TimeEntryWrapper.class);
+//		
+//		int checkVar = this.limit;
+//		int offset = 0;
+//		while (checkVar == this.limit && offset < 1000){
+//			final List<TimeEntry> part = checkDates(wrapper, startDate, endDate);
+//			checkVar = part.size();
+//			out.addAll(part);
+//			log.info("Read " + part.size() + " elements and adding to store. " + out.size() + " entries in total");
+//			final List<String> tempParams = params.subList(0, 3);
+//			offset=offset+100;
+//			tempParams.add("offset=" + offset);
+//			wrapper = RMService.getResponse(format, "time_entries", tempParams, TimeEntryWrapper.class);
+//		}
+//		
+//		return out;
+//	}
 
-		List<TimeEntry> out = new ArrayList<>();
-		
-		LocalDate startDate = null, endDate = null;
-
-		try {
-			startDate = parseDate(startDateString);
-			endDate = parseDate(endDateString);
-		} catch (IllegalArgumentException e) {
-			log.warn(e.getMessage());
-			return null;
-		}
-
-		List<String> params = new ArrayList<>();
-		params.add("spent_on=>=" + startDateString);
-		params.add("spent_on=<=" + endDateString);
-		params.add("limit=" + limit);
-		TimeEntryWrapper wrapper = RMService.getResponse(format, "time_entries", params, TimeEntryWrapper.class);
-		
-		int checkVar = this.limit;
-		int offset = 0;
-		while (checkVar == this.limit && offset < 1000){
-			final List<TimeEntry> part = checkDates(wrapper, startDate, endDate);
-			checkVar = part.size();
-			out.addAll(part);
-			log.info("Read " + part.size() + " elements and adding to store. " + out.size() + " entries in total");
-			final List<String> tempParams = params.subList(0, 3);
-			offset=offset+100;
-			tempParams.add("offset=" + offset);
-			wrapper = RMService.getResponse(format, "time_entries", tempParams, TimeEntryWrapper.class);
-		}
-		
-		return out;
-	}
-
-	private List<TimeEntry> checkDates(TimeEntryWrapper wrapper, LocalDate startDate, LocalDate endDate)
-			throws IllegalArgumentException {
-
-		List<TimeEntry> out = new ArrayList<TimeEntry>();
-		
-		for (TimeEntry entry : wrapper.getTime_entries()) {
-			try {
-				LocalDate entryDate = LocalDate.parse(entry.getSpent_on());
-				if ((entryDate.equals(startDate) || entryDate.equals(endDate))
-						|| (entryDate.isBefore(endDate) && entryDate.isAfter(startDate))
-				) {
-					out.add(entry);
-				}
-
-			} catch (DateTimeParseException e) {
-				log.warn("Could not parse the date: " + entry.getSpent_on());
-			}
-		}
-
-		return out;
-	}
+//	private List<TimeEntry> checkDates(TimeEntryWrapper wrapper, LocalDate startDate, LocalDate endDate)
+//			throws IllegalArgumentException {
+//
+//		List<TimeEntry> out = new ArrayList<TimeEntry>();
+//		
+//		for (TimeEntry entry : wrapper.getTime_entries()) {
+//			try {
+//				LocalDate entryDate = LocalDate.parse(entry.getSpent_on());
+//				if ((entryDate.equals(startDate) || entryDate.equals(endDate))
+//						|| (entryDate.isBefore(endDate) && entryDate.isAfter(startDate))
+//				) {
+//					out.add(entry);
+//				}
+//
+//			} catch (DateTimeParseException e) {
+//				log.warn("Could not parse the date: " + entry.getSpent_on());
+//			}
+//		}
+//
+//		return out;
+//	}
 
 	private LocalDate parseDate(String input) throws IllegalArgumentException {
 		try {
